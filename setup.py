eb351639af9fff4b754b43ab8fd2d766af2d733d
FingerLiu <liupeng.dalian@gmail.com>
2025-05-21 14:36:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=3.11",
+    python_requires=">=3.10",
     install_requires=[
         "click",
         "mcp", 
